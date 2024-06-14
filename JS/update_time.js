8a3eeea32da031f81ca81d1d5886f90b369b22c7
K_tan <yuikatayama0525@gmail.com>
2024-06-14 14:26:44 +0900

    時間表示を追加

---
+++ JS/update_time.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function updateTime() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1; // 月は0から始まるため、+1します
+        const date = now.getDate();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+
+        
+        const formattedTime = `${year}年${month}月${date}日 ${hours}時${minutes}分${seconds}秒 `;
+        
+        document.getElementById('time').textContent = formattedTime;
+    }
+
+    setInterval(updateTime, 50);
+    updateTime();
+});
