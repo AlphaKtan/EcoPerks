8dec5892738a9bc6dd867d362c051c3ca85422f0
K_tan <yuikatayama0525@gmail.com>
2024-06-26 10:39:26 +0900

    デバッグ用追加

--- PHP/2FA_2.php
+++ PHP/2FA_2.php
@@ -1,57 +1,57 @@
 <?php
-
 // セッションの開始
 session_start();
 
 header('Content-Type: text/html; charset=utf-8');
 
-
-
 // セッションから認証コードを取得
 $verificationCode = isset($_SESSION['verification_code']) ? $_SESSION['verification_code'] : '';
+
+// デバッグ用の出力
+// echo "Session Verification Code: " . $verificationCode;
 ?>
 
-<!-- 2FA_2.php -->
-
-<!-- 二段階認証コードの入力画面の処理 -->
+<!DOCTYPE html>
 <html lang="jp">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="#">
-        <title>2ファクタ認証</title>
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="#">
+    <title>2ファクタ認証</title>
+</head>
+<body>
 
-        <h2 class="h2">2ファクタ認証</h2>
-        <div class="#">
-            <h2 class="#">認証コードをecoparks202404@gmail.comからご登録いただいたメールに送信しました。<br>受信箱にない場合は迷惑メールフォルダの中などをご確認ください。</h2>
-            
-            <?php
-            // POST データの処理
-            if (isset($_POST['submit'])) {
-                $userEnteredCode = $_POST['verification_code'];
+<h2 class="h2">2ファクタ認証</h2>
+<div class="#">
+    <h2 class="#">認証コードをecoparks202404@gmail.comからご登録いただいたメールに送信しました。<br>受信箱にない場合は迷惑メールフォルダの中などをご確認ください。</h2>
+    
+    <?php
+    // POST データの処理
+    if (isset($_POST['submit'])) {
+        $userEnteredCode = $_POST['verification_code'];
 
-                // 入力されたコードとセッションから取得したコードを比較
-                if ($userEnteredCode == $verificationCode) {
-                    // 認証成功したとき
-                    header("Location: ../index.html");
-                    exit;
-                } else {
-                    // 認証失敗したとき
-                    echo '<h2 style="color: red;">認証コードが正しくありません。</h2>';
-                    echo "<h2><a href='../login.html'>ログインページよりもう一度実行してください</a></h2>";
-                }
-            }
-            ?>
-            
-            <hr>
-            <form method="post" action="" class="#">
-                <label for="verification_code" size>6桁の認証コードを入力</label>
-                <h5 class="#">↓　↓　↓</h5>
-                <input type="text" id="verification_code" name="verification_code" required ><br>
-                <button type="submit" name="submit" class="example">認証する</button>
-            </form>
-        </div>
-    </body>
+        // 入力されたコードとセッションから取得したコードを比較
+        if ($userEnteredCode == $verificationCode) {
+            // 認証成功したとき
+            header("Location: ../index.html");
+            exit;
+        } else {
+            // 認証失敗したとき
+            echo '<h2 style="color: red;">認証コードが正しくありません。</h2>';
+            echo "<h2><a href='../login.html'>ログインページよりもう一度実行してください</a></h2>";
+        }
+    }
+    ?>
+    
+    <hr>
+    <form method="post" action="" class="#">
+        <label for="verification_code">6桁の認証コードを入力</label>
+        <h5 class="#">↓　↓　↓</h5>
+        <input type="text" id="verification_code" name="verification_code" required><br>
+        <button type="submit" name="submit" class="example">認証する</button>
+    </form>
+</div>
+
+</body>
 </html>
+
