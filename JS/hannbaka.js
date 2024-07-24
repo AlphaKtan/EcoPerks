d5dfef7f3de457d1eaa0b690c71fcd7bc37c4e2e
revers <emall@email.com>
2024-07-24 16:38:13 +0900

    検証ツールにてg-navがうざい問題解決、元に戻るボタンをmapの中に入れた

--- JS/hannbaka.js
+++ JS/hannbaka.js
@@ -1,14 +1,16 @@
-$(".openbtn").click(function () {//ボタンがクリックされたら
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+$(".openbtn").click(function () {
+    $(this).toggleClass('active'); // ボタン自身に active クラスを付与
+    $("#g-nav").toggleClass('panelactive');
+
+    if ($("#g-nav").hasClass('panelactive')) {
+        $("#g-nav").css('height', '100vh'); // panelactive クラスがある場合に高さを設定
+    } else {
+        $("#g-nav").css('height', ''); // panelactive クラスがない場合に高さをリセット
+    }
 });
 
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+$("#g-nav a").click(function () {
+    $(".openbtn").removeClass('active'); // ボタンの active クラスを除去
+    $("#g-nav").removeClass('panelactive'); // ナビゲーションの panelactive クラスも除去
+    $("#g-nav").css('height', ''); // ナビゲーションの高さをリセット
 });
-
-
-
-
-
