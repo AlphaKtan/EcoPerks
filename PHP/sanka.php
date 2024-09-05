14fe9898506d9eb6bb482bbed9bab661df09ad65
yuikatayama <yuikatayama0525@gmail.com>
2024-09-05 15:45:07 +0900

    no message

--- PHP/sanka.php
+++ PHP/sanka.php
@@ -1,11 +1,20 @@
 <?php
-require_once('../vendor/autoload.php');
+require_once('vendor/autoload.php');
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Encoding\Encoding;
-use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Label\Label;
+use Endroid\QrCode\Logo\Logo;
+
+
 
 // サーバーURLを設定（開始用と終了用で異なる）
 $baseUrl = 'http://i2322117.chips.jp/php/gomiclean.php';
@@ -19,13 +28,12 @@ $endUrl = $baseUrl . '?location_id=10&action=end';
 // QRコード生成関数
 function generateQrCode($url) {
     $qrCode = QrCode::create($url)
-        ->setEncoding(new Encoding('UTF-8'))
-        ->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
-        ->setSize(200)
-        ->setMargin(10)
-        ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
-        ->setForegroundColor(new Color(0, 0, 0));
-
+            ->setEncoding(new Encoding('UTF-8'))
+            ->setErrorCorrectionLevel(ErrorCorrectionLevel::Low)
+            ->setSize(150)
+            ->setMargin(25)
+            ->setRoundBlockSizeMode(RoundBlockSizeMode::Margin)
+            ->setForegroundColor(new Color(0, 0, 0));
     $writer = new PngWriter();
     $result = $writer->write($qrCode);
 
