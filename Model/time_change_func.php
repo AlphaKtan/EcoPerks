101536ac7268325852f6da4a0901d2746aa5884f
revers <emall@email.com>
2024-10-02 16:39:07 +0900

    途中

--- Model/time_change_func.php
+++ Model/time_change_func.php
@@ -1,11 +1,22 @@
-<?php 
-function getFacility_name($area) {
+<?php
+function getFacility_name($pdo,$area) {
     // travel_dataからfacility_nameを取得
     $locationsql = "SELECT id, area_id, facility_name FROM travel_data WHERE area_id = :area_id";
     $locationstmt = $pdo->prepare($locationsql);
     $locationstmt->bindParam(':area_id', $area, PDO::PARAM_INT);
     $locationstmt->execute();
-    $locationRow = $locationstmt->fetchAll(PDO::FETCH_ASSOC);
+    return $locationstmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function timeDisplay($pdo, ) {
+    // 各facility_nameごとに関連するtime_changeのデータを取得
+    $sql = "SELECT status, start_time, end_time, facility_name FROM time_change WHERE facility_name = :facility_name AND areaid = :area_id AND status = :status";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':facility_name', $k['facility_name'], PDO::PARAM_STR);
+    $stmt->bindParam(':area_id', $area, PDO::PARAM_INT);
+    $stmt->bindParam(':status', $status, PDO::PARAM_INT);
+    $stmt->execute();
+    $areaRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 ?>
--- PHP/get_time_chage.php
+++ PHP/get_time_chage.php
@@ -10,11 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['area'])) {
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // travel_dataからfacility_nameを取得
-        $locationsql = "SELECT id, area_id, facility_name FROM travel_data WHERE area_id = :area_id";
-        $locationstmt = $pdo->prepare($locationsql);
-        $locationstmt->bindParam(':area_id', $area, PDO::PARAM_INT);
-        $locationstmt->execute();
-        $locationRow = $locationstmt->fetchAll(PDO::FETCH_ASSOC);
+        $locationRow = getFacility_name($pdo,$area);
 
         $status = 1;
         if ($locationRow) {
