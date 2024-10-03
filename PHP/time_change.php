bfe4a6f9ce4a54d00f0b2a3c497baddce28a9036
revers <emall@email.com>
2024-10-03 15:01:44 +0900

    途中

--- PHP/time_change.php
+++ PHP/time_change.php
@@ -1,64 +1,56 @@
 <select name="area" id="areaSelect">
-    <option  hidden value="">選択してください</option>
+    <option hidden value="">選択してください</option>
     <option value="">すべて</option>
     <?php 
-    for($i=1; $i <= 25; $i++){
-        echo '<option value="' . $i . '">' . 'エリア'. $i .'</option>';
+    for ($i = 1; $i <= 25; $i++) {
+        echo '<option value="' . $i . '">' . 'エリア' . $i . '</option>';
     }
     ?>
 </select>
-
-<select name="facility_name" id="">
-    <option  hidden value="">選択してください</option>
-    <option value="">すべて</option>
+<br>
+<br>
+<br>
+<select name="facility_name" id="facilitySelect">
+    <option hidden value="">選択してください</option>
 </select>
+<br>
+<br>
+<br>
+<input type="date" id="reservation_date" name="reservation_date">
 
 <div id="areaInfo"></div>
 
 <script>
+// 施設名と時間帯をマッピングするためのオブジェクト
+let facilityMap = {};
+let jsonData = [];
+
 document.getElementById('areaSelect').addEventListener('change', function() {
-    var areaValue = this.value;
-    
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'get_time_chage.php', true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.send('area=' + areaValue);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                try {
-                    const jsonData = JSON.parse(xhr.responseText); // JSONをパース
-                
-                    // 施設名と時間帯をマッピングするためのオブジェクト
-                    let facilityMap = {};
+    let selectedFacility = this.value;
 
-                    jsonData.forEach(item => {
-                        // facility_infoとtime_infoが存在するかを確認
-                        if (item.facility_info && item.time_info) {
-                            let facilityName = item.facility_info.facility_name;
-                            let timeRange = item.time_info.start_time + ' ～ ' + item.time_info.end_time + ' 状態 ' + item.time_info.status;
+    document.getElementById('areaInfo').innerHTML = '';
 
-                            // 施設名がまだマップに登録されていなければ、初期化
-                            if (!facilityMap[facilityName]) {
-                                facilityMap[facilityName] = new Set(); // 重複を排除するためにSetを使用
-                            }
-                            // 時間帯をセットに追加（重複しないように）
-                            facilityMap[facilityName].add(timeRange);
-                        }
-                    });
-
-                    // 施設情報をHTMLに整形
-                    let facilityInfo = Object.keys(facilityMap).map(function(facilityName) {
-                        let times = Array.from(facilityMap[facilityName]).join('<br>'); // Setを配列に変換して時間帯を連結
-                        return '<strong>' + facilityName + '</strong><br>' + times; // 施設名と時間帯をまとめる
-                    }).join('<br><br>'); // 各施設情報の間に改行を追加
-
-                    // HTMLを更新
-                    document.getElementById('areaInfo').innerHTML = facilityInfo;
-                } catch (error) {
-                    document.getElementById('areaInfo').innerHTML = xhr.responseText;
-                }
-
-            }
-        };
+    if (facilityMap[selectedFacility]) {
+        let times = Array.from(facilityMap[selectedFacility]).join('<br>'); // Setを配列に変換して時間帯を連結
+        document.getElementById('areaInfo').innerHTML = '<strong>' + selectedFacility + '</strong><br>' + times;
+    }   
+    ariaTimeFilter();
 });
+
+document.getElementById('facilitySelect').addEventListener('change', function() {
+    ariaTimeFilter();
+});
+
+document.getElementById('reservation_date').addEventListener('change', function() {
+    ariaTimeFilter();
+});
+
+function ariaTimeFilter() {
+    alert("ああああ");
+}
+
+
+
+
+
 </script>
