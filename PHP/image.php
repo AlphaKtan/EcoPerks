47fb7a8464a9b88be88692bbdc929f17356665c1
matcha0609 <2322086@i-seifu.jp>
2024-10-21 10:24:10 +0900

    途中経過４

--- PHP/image.php
+++ PHP/image.php
@@ -34,26 +34,4 @@ if (isset($_SESSION['user_id'])) {
 <img src="../images/<?php echo $image['imgpath']; ?>" width="300" height="300">
 <a href="upload.php">画像アップロード</a>
 
-<script>
-    let inputText = ''; // 入力されたテキストを保持する変数
-
-    // body全体にキー入力を検出するリスナーを設定
-    document.body.addEventListener('keydown', function(event) {
-        // Enterキーが押されたかどうかを確認
-        if (event.key === 'Enter') {
-            event.preventDefault(); // Enterキーのデフォルト動作を防止
-            console.log(inputText);
-            // 入力されたテキストが "[start]" ならリダイレクト
-            if (inputText === 'start') {
-                window.location.href = './start_user.php'; // リダイレクト先のURL
-            }
-
-            // 入力をリセット
-            inputText = '';
-        } else if (event.key.length === 1) {
-            // アルファベットや数字などの1文字のキーが押された場合のみ追加
-            inputText += event.key;
-            
-        }
-    });
-</script>
+<script src="../JS/pass.js"></script>
