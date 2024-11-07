28a40a36b5ab7e341e12823488fa08caf53af7b5
revers <emall@email.com>
2024-11-07 16:48:00 +0900

    SQL追加

---
+++ preset.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2024-11-07 08:47:01
+-- サーバのバージョン： 10.4.32-MariaDB
+-- PHP のバージョン: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `ecoperks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `preset`
+--
+
+CREATE TABLE `preset` (
+  `id` int(11) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `preset`
+--
+
+INSERT INTO `preset` (`id`, `start_time`, `end_time`) VALUES
+(1, '09:00:00', '10:00:00'),
+(2, '10:00:00', '11:00:00'),
+(3, '11:00:00', '12:00:00'),
+(4, '12:00:00', '13:00:00'),
+(5, '13:00:00', '14:00:00'),
+(6, '14:00:00', '15:00:00'),
+(7, '16:00:00', '17:00:00');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `preset`
+--
+ALTER TABLE `preset`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `preset`
+--
+ALTER TABLE `preset`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
