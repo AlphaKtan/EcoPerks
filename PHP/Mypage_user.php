24fd08344a34622b98b46cb2ce730b2deaa48026
revers <emall@email.com>
2024-12-11 11:54:36 +0900

    らいるのやつもあげた

--- PHP/Mypage_user.php
+++ PHP/Mypage_user.php
@@ -69,169 +69,78 @@
         <div class="icon"></div>
     </div>
     </header>
+
     <div class="sub_header">
-        <div class="sub_header_box1"></div>
-        
-        <div class="sub_header_box2"></div>
+        <div class="sub_header_box1">
+            <p style="padding-left: 10px;">登録状況</p>
+        </div>
+        <div class="sub_header_box2" style="border-left:solid 1px #ffff;">
+        </div>
     </div>
-    <div class="profile">
-        <div class="userFlex">
-            <div class="userFlexItem">
-                <div id="dragDropArea">
-                    <div class="drag-drop-inside">
-                        <div id="previewArea">
-                        <img src="../images/<?php echo $image['imgpath']; ?>" width="300" height="300" class="iconImg">
-                        </div>
+
+    <div class="userFlexItem">
+        <a href="./upload.php">
+            <div id="dragDropArea">
+                <div class="drag-drop-inside">
+                    <div id="previewArea">
+                    <img src="../images/<?php echo $image['imgpath']; ?>" width="146" height="140" class="iconImg">
                     </div>
                 </div>
             </div>
+        </a>
+    </div>
 
-            <div class="userFlexItem">
-            <a href="./upload.php">
-                <h2 class="center">
-                    <?php
-                    if($row){
-                        foreach($row as $rows){
-                    $username = $rows['username'];
-                     echo "$username";
-                        }
-                    }?>
-                </h2>
-            </a>
-            </div>
-            <div class="userFlexItem">
-                <h2 class="center">現在のポイント</h2>
-                <div class="pointArea">
-                    <img src="../img/point.jpg" alt="" class="point">
-                    <h4>？？pt</h4>
-                </div>
-            </div>
-        </div>
+    <div class="userFlexItem">
+        <h2 class="center">ユーザー名</h2>
     </div>
     
-    <div class="informationBox">
-        <div class="boxA">
-            
-            
-        </div>
-        <div class="boxA">
+    <div class="userFlexItem">
+        <a href="./upload.php">
+        <h2 class="center">
+            <?php
+                if($row){
+                    foreach($row as $rows){
+                $username = $rows['username'];
+                    echo "$username";
+                    }
+                }
+            ?>
+        </h2>
+        </a>
+    </div>
+
+    <div class="boxA">
+        <div class="boxB">
             <div class="box3">
-            <a href="./coupons.php">
-                <h1>クーポン</h1>
-                <p>Conpon</p>
+                <a href="./coupons.php">
+                    <h1>クーポン</h1>
+                    <p>Conpon</p>
                 </a>
             </div>
-            <a href="ReserveCheck_Customer.php">
-            <div class="box4">
-                <h1>予約確認</h1>
-                <p>CheckReserve</p>
-            </div>
-            </a>
-        </div>
-        <div class="boxA">
+
             <div class="box3">
+                <a href="ReserveCheck_Customer.php">
+                    <h1>予約確認</h1>
+                    <p>CheckReserve</p>
+                </a>
+            </div>
+        </div>
+        <div class="box5">
             <a href="./upload.php">
                 <h1>名前変更</h1>
                 <p>ChangeName</p>
-                </a>
-            </div>
-            <a href="../index.html">
-            <div class="box4">
-                <h1>ホームに戻る</h1>
-                <p>back home</p>
-            </div>
             </a>
         </div>
     </div>
 
- 
-    
-    <div class="test" style="height: 700px;"></div>
-    <!-- <footer>
-        <div id="flexBox02">
-            <div class="flexItem02">
-                <h4>Mission</h4>
-                <div class="progress">
-                    <div class="progress-bar" style="width: 80%;"></div>
-                </div>
-            </div>
-            <div class="flexItem02">
-                <h4>QR</h4>
-                <img src="../img/qr.png"width="45px" height="45px">
-            </div>
-            <div class="flexItem02">
-                <h4>Ranking</h4>
-                <img src="../img/ranking.png"width="50px" height="30px">
-            </div>
-        </div>
-    </footer> -->
+    <div class="QR_botton">
+        <img src="..\img\qr_white.png" class="QRimg">
+    </div>
+
+
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<!--自作のJS-->
-<script src="../JS/hannbaka.js"></script>
-
-<script>
-    let fileArea = document.getElementById('dragDropArea');
-    let fileInput = document.getElementById('fileInput');
-
-    fileArea.addEventListener('dragover', function(evt){
-        evt.preventDefault();
-        fileArea.classList.add('dragover');
-    });
-
-    fileArea.addEventListener('dragleave', function(evt){
-        evt.preventDefault();
-        fileArea.classList.remove('dragover');
-    });
-
-    fileArea.addEventListener('drop', function(evt){
-        evt.preventDefault();
-        fileArea.classList.remove('dragenter');
-        let files = evt.dataTransfer.files;
-        fileInput.files = files;
-        photoPreview('onChange',files[0]);
-    });
-
-    function photoPreview(event, f = null) {
-        let file = f;
-        if(file === null){
-            file = event.target.files[0];
-        }
-        let reader = new FileReader();
-        let previewImage = document.getElementById("previewImage");
-        let fileInputLabel = document.getElementById('fileInputLabel')
-
-        // 許可する拡張子以外の場合
-        if (file && !isValidFile(file)) {
-            alert('拡張子が jpeg, jpg, png, bmp, gif 以外のファイルはアップロードできません。');
-
-            resetFileInput();
-            return; // 処理を中断
-        }
-
-        reader.onload = function(event) {
-            let img = document.createElement("img");
-            img.alt = "User's Avatar"
-            img.setAttribute('class', "nav-user-photo-large")
-            img.setAttribute("src", reader.result);
-            img.setAttribute("id", "previewImage");
-            fileInputLabel.replaceChild(img, previewImage);
-        };
-
-        reader.readAsDataURL(file);
-    }
-
-    function isValidFile(file) {
-        const allowExtensions = '.(jpeg|jpg|png|bmp|gif)$'; // 許可する拡張子
-
-        return file.name.match(allowExtensions)
-    }
-
-    function resetFileInput() {
-        document.getElementById('fileInput').value = '';
-    }
-</script>
-
-<script src="../JS/pass.js"></script>
+    <script src="../JS/hannbaka.js"></script>
+    <script src="../JS/pass.js"></script>
 
 
 </body>
--- PHP/yoyaku.php
+++ PHP/yoyaku.php
@@ -1,48 +1,12 @@
 <?php 
 session_start();
 
-if (isset($_POST['location'])) {
-    $location = $_POST['location'];
+if (isset($_GET['location'])) {
+    $location = $_GET['location'];
     $_SESSION['location'] = $location;
 }
 
-
-// } else {
-//     throw new Exception("locationが指定されていません。");
-// }
-
 ?>
-<!-- <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/indexStyle.css">
-    <link rel="stylesheet" href="../CSS/yoyakuStyle.css">
-    <title>予約フォーム</title>
-</head>
-<body>
-
-    <header>
-        <div class="flexBox">
-            <div class="menu">
-            </div>
-            <div class="logo">
-                <img src="../img/logo.jpg" alt="" class="logo2">
-            </div>
-            <div class="icon"></div>
-        </div>
-    </header>
-
-    <form class="yoyaku_form" action="yoyaku_output.php" method="post">
-        <h1>予約フォーム</h1>
-        <label for="reservation_date">予約日:</label>
-        <input type="date" id="reservation_date" name="reservation_date" required>
-        <input type="submit" value="時間を確認">
-    </form>
-</body>
-</html> -->
-
 
 <?php
 require '../Model/dbModel.php';
@@ -164,7 +128,7 @@ for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
 <body>
     <div class="container mt-5">
         <h3 class="mb-4">
-            <a href="?ym=<?= $prev ?>">&lt;</a>
+            <a href="?ym=<?= $prev ?>&location=<?= $prev ?>">&lt;</a>
             <span class="mx-3">
                 <?= $html_title ?>
             </span>
