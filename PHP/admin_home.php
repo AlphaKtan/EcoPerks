1311f164bfa9b915f25cff0c8a32d042fc9bde00
R202-202\lightbox2 <zhiyegongxia25@gmail.com>
2024-12-18 09:27:51 +0900

    ともやマージ

--- PHP/admin_home.php
+++ PHP/admin_home.php
@@ -4,44 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>管理者向け総合ページ</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-        }
-        h1 {
-            font-size: 36px; /* タイトルの文字サイズ */
-            margin-bottom: 30px;
-        }
-        ul {
-            list-style-type: none;
-            padding: 0;
-        }
-        li {
-            margin: 15px 0;
-        }
-        a {
-            text-decoration: none;
-            color: #007bff;
-            font-size: 24px; /* リンクの文字サイズ */
-        }
-        a:hover {
-            text-decoration: underline;
-        }
-    </style>
+    <link rel="stylesheet" href="../CSS/hannbaka.css">
+    <link rel="stylesheet" href="../CSS/sidebar.css">
 </head>
 <body>
-    <h1>管理者向け総合ページ</h1>
-    <ul>
-        <li><a href="admin.php">管理者ページ</a></li>
-        <li><a href="adminqr.php">QRコード生成ページ</a></li>
-        <li><a href="adminqr_end.php">QRコード終了管理</a></li>
-        <li><a href="data.php">データ管理ページ</a></li>
-        <li><a href="access_log.php">アクセスログ表示ページ</a></li>
-        <li><a href="sankakanri.php">参加者管理ページ</a></li>
-        <li><a href="shift.php">シフトページ</a></li>
-        <li><a href="time_change.php">シフト削除</a></li>
-    </ul>
+<div class="Amozon-container">
+    <div class="left-menu">
+        <div>
+            <ul class="menu-list">
+                <p class="text-box">アカウント情報</p>
+                <li class="menu-item"><a href="admin.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">ログイン</span></a></li>
+                <p class="text-box">QRコード</p>
+                <li class="menu-item"><a href="adminqr.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">QRコード生成ページ</span></a></li>
+                <li class="menu-item"><a href="adminqr_end.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">QRコード終了管理</span></a></li>
+                <p class="text-box">その他/管理・表示</p>
+                <li class="menu-item"><a href="data.php" class="a_link"><img src="../img/qr.png" class="logo"></span><span class="menu-item-text-chat">データ管理ページ</span></a></li>
+                <li class="menu-item"><a href="sankakanri.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">参加者管理ページ</span></a></li>
+                <li class="menu-item"><a href="shift.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">シフトページ</span></a></li>
+                <li class="menu-item"><a href="time_change.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">シフト削除</span></a></li>
+                <li class="menu-item"><a href="access_log.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">アクセスログ表示ページ</span></a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="menu-list-bottom">
+            </ul>
+        </div>
+    </div>
+    <div class="right-content">
+</div>
+</ul>
 </body>
 </html>
-
--- PHP/adminqr.php
+++ PHP/adminqr.php
@@ -26,23 +26,62 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['facility_id'])) {
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
+    <link rel="stylesheet" href="../CSS/hannbaka.css">
+    <link rel="stylesheet" href="../CSS/adminqr.css">
     <title>管理者様施設選択ページ</title>
-</head>
+</head>    
 <body>
-    <h2>施設を選択してください</h2>
-    <form action="" method="post"> <!-- 現在のページにPOSTリクエストを送信 -->
-        <label for="facility_id">施設名:</label>
-        <select name="facility_id" id="facility_id">
-            <?php foreach ($facilities as $facility): ?>
-                <option value="<?php echo htmlspecialchars($facility['id'], ENT_QUOTES, 'UTF-8'); ?>">
-                    <?php echo htmlspecialchars($facility['facility_name'], ENT_QUOTES, 'UTF-8'); ?>
-                </option>
-            <?php endforeach; ?>
-        </select>
-        <br><br>
-        <button type="submit">選択</button>
+
+    <header>
+        <div class="flexBox">
+            <div class="menu">
+                <div class="openbtn"><span></span><span></span><span></span></div>
+                <nav id="g-nav">
+                    <div id="g-nav-list"><!--ナビの数が増えた場合縦スクロールするためのdiv※不要なら削除-->
+                        <ul>
+                            <li><a href="#">Top</a></li>
+                            <li><a href="../login_page.php">ログイン</a></li> 
+                            <li><a href="regist.php">アカウント作成</a></li> 
+                            <li><a href="Mypage_user.php">Mypage</a></li> 
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div class="logo">
+                <img src="../img/logo.jpg" alt="" class="logo2">
+            </div>
+            <div class="icon">
+                <form action="logout.php" method="post" class = "logout_form">
+                    <button type="submit" class="logout1">ログアウト</button>
+                </form>
+            </div>
+        </div>
+    </header>    
+    <h2 class="search">エリア検索</h2>
+    <div class="A1">
+        <p class="area">施設名選択</p>
+        <br>
+            <form action="" method="post"> <!-- 現在のページにPOSTリクエストを送信 -->
+                <select name="facility_id" id="facility_id">
+                <option value="" selected disabled>=== 選択してください ===</option>
+                    <?php foreach ($facilities as $facility): ?>
+                        <option value="<?php echo htmlspecialchars($facility['id'], ENT_QUOTES, 'UTF-8'); ?>">
+                            <?php echo htmlspecialchars($facility['facility_name'], ENT_QUOTES, 'UTF-8'); ?>
+                        </option>
+                    <?php endforeach; ?>    
+                </select>
+                <br>
+            <p>※QR発行後、参加者が読み取り次第、ゴミ拾いが開始します。</p>
+                <br>
+            <div class="button-container">
+             <button type="submit" class="select-btn">選択する</button>
+            </div>
+        
+        </div>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     </form>
+    <script src="../JS/hannbaka.js"></script>
 </body>
 </html>
 
