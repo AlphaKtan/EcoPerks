7f098b8483059e6abf44aff63d21c15403d273d9
river-s <yuuki0624kawano@mail.com>
2024-12-18 22:03:18 +0900

    innerHTMLが正しくdivを認識していなかったので認識されるようにした

--- PHP/shift.php
+++ PHP/shift.php
@@ -104,9 +104,6 @@ $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>シフト登録画面</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../CSS/shiftStyle.css">
-    <style>
-
-</style>
 </head>
 <body>
     <div class="container mt-5">
