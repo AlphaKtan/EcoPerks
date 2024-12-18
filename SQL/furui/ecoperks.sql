58f8cc898ce5d6afc3d5d89e008e8eb81a039373
river-s <yuuki0624kawano@mail.com>
2024-12-18 21:23:47 +0900

    データベース最新

--- SQL/furui/ecoperks.sql
+++ SQL/furui/ecoperks.sql
@@ -1,11 +1,11 @@
-  -- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2024-12-11 01:23:58
+-- 生成日時: 2024-12-18 07:16:31
 -- サーバのバージョン： 10.4.32-MariaDB
--- PHP のバージョン: 8.2.12
+-- PHP のバージョン: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -92,6 +92,49 @@ INSERT INTO `coupons` (`coupon_id`, `username`, `coupon_code`, `discount`, `expi
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `map`
+--
+
+CREATE TABLE `map` (
+  `id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `coord` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `map`
+--
+
+INSERT INTO `map` (`id`, `area_id`, `coord`) VALUES
+(1, 1, '(9196, 4151)'),
+(2, 2, '(9197, 4151)'),
+(3, 3, '(9194, 4152)'),
+(4, 4, '(9195, 4152)'),
+(5, 5, '(9196, 4152)'),
+(6, 6, '(9197, 4152)'),
+(7, 7, '(9198, 4152)'),
+(8, 8, '(9195, 4153)'),
+(9, 9, '(9196, 4153)'),
+(10, 10, '(9197, 4153)'),
+(11, 11, '(9198, 4153)'),
+(12, 12, '(9194, 4154)'),
+(13, 13, '(9195, 4154)'),
+(14, 14, '(9196, 4154)'),
+(15, 15, '(9197, 4154)'),
+(16, 16, '(9198, 4154)'),
+(17, 17, '(9194, 4155)'),
+(18, 18, '(9195, 4155)'),
+(19, 19, '(9196, 4155)'),
+(20, 20, '(9197, 4155)'),
+(21, 21, '(9198, 4155)'),
+(22, 22, '(9195, 4156)'),
+(23, 23, '(9196, 4156)'),
+(24, 24, '(9197, 4156)'),
+(25, 25, '(9198, 4156)');
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `preset`
 --
 
@@ -163,21 +206,43 @@ CREATE TABLE `survey_responses` (
   `gomi` tinyint(4) NOT NULL,
   `body` text NOT NULL,
   `image_path` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `areaid` int(11) DEFAULT NULL,
+  `coord` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- テーブルのデータのダンプ `survey_responses`
 --
 
-INSERT INTO `survey_responses` (`survey_id`, `gomi`, `body`, `image_path`, `created_at`) VALUES
-(1, 1, 'ゴミが多いので、もっと回収頻度を上げてほしい', '../images/sample1.jpg', '2024-12-02 08:52:54'),
-(2, 2, 'ゴミの量はまぁまぁです。問題ありません。', NULL, '2024-12-02 08:52:54'),
-(3, 3, 'ゴミは少ないですが、ゴミ箱が不足しています。', '../images/sample2.jpg', '2024-12-02 08:52:54'),
-(4, 2, 'hhlkkhjkjhlkjh', '../images/1185888840674d77aa22b210.23011545.jpg', '2024-12-02 09:02:35'),
-(5, 2, 'だだｆｓふぁｄｄふぁあｆｄ', '../images/1181778231674d7ac8e35767.41136090.jpg', '2024-12-02 09:15:53'),
-(6, 2, 'だだｆｓふぁｄｄふぁあｆｄ', '../images/683246905674d7aea7a8622.83305329.jpg', '2024-12-02 09:16:27'),
-(7, 2, 'だだｆｓふぁｄｄふぁあｆｄ', '../images/196097546674d7b32dc7f39.72669828.jpg', '2024-12-02 09:17:39');
+INSERT INTO `survey_responses` (`survey_id`, `gomi`, `body`, `image_path`, `created_at`, `areaid`, `coord`) VALUES
+(8, 2, 'ｄｇｓｆｚｃヴぁｈｆｄｇんｂｖ86120', '../images/1352344694675a4e39e32cd2.31628963.png', '2024-12-12 02:48:53', 18, ''),
+(9, 1, 'ｄｇｓｆｚｃヴぁｈｆｄｇんｂｖ86120', '../images/1393892183675a4f187e05d3.14164069.png', '2024-12-12 02:48:57', 18, ''),
+(10, 4, 'ｄｇｓｆｚｃヴぁｈｆｄｇんｂｖ86120', '../images/933247600675a4f2ea82004.55544586.png', '2024-12-12 02:49:19', 25, ''),
+(11, 2, 'wezsxrdctfvyg54156\r\n341+4', '../images/1709066249675a4fde5bb9c4.87126838.png', '2024-12-12 02:52:15', 24, ''),
+(12, 4, 'fadf', '../images/1732946465676233b4f19d20.25033385.png', '2024-12-18 02:30:14', 18, ''),
+(13, 4, 'fadf', '../images/1493600699676233c1cc06e4.56042968.png', '2024-12-18 02:30:26', 18, ''),
+(14, 3, 'dsafadfdafsafds', '', '2024-12-18 02:31:37', 10, ''),
+(15, 3, 'dsafadfdafsafds', '', '2024-12-18 02:32:45', 10, ''),
+(16, 2, 'dsafadfdafsafds', '', '2024-12-18 02:35:02', 10, ''),
+(17, 2, 'dsafadfdafsafds', '', '2024-12-18 02:35:28', 10, ''),
+(18, 1, 'dsafadfdafsafds', '', '2024-12-18 02:36:55', 10, ''),
+(19, 4, 'あｆｄｓ', '../images/3886974056762359be07cf4.37616003.jpg', '2024-12-18 02:38:21', 14, ''),
+(20, 4, 'あｆｄｓ', '../images/9433489906762374c62f385.34458878.jpg', '2024-12-18 02:45:33', 14, ''),
+(21, 4, 'あｆｄｓ', '../images/1487805760676238a5271ed6.71717330.jpg', '2024-12-18 02:51:18', 1, ''),
+(22, 1, 'あｆｄｓ', '../images/695983043676238bb2935e5.27946576.jpg', '2024-12-18 02:51:39', 1, ''),
+(23, 4, 'あｆｄｓ', '../images/879806517676239c9f17f84.79768150.jpg', '2024-12-18 02:56:10', 6, ''),
+(24, 3, 'あｆｄｓ', '../images/1433065601676239e9248a09.89611850.jpg', '2024-12-18 02:56:41', 7, ''),
+(25, 1, 'あｆｄｓ', '../images/395645259676239f436cb17.71328292.jpg', '2024-12-18 02:56:52', 7, ''),
+(26, 4, 'あｆｄｓ', '../images/57262381067623a1bcee282.21372220.jpg', '2024-12-18 02:57:32', 10, ''),
+(27, 3, 'あｆｄｓ', '../images/20989200167623a3bef33b6.46097920.jpg', '2024-12-18 02:58:05', 10, ''),
+(28, 4, 'あｆｄｓ', '../images/127771752467623a487faba0.52994021.jpg', '2024-12-18 02:58:17', 10, ''),
+(29, 4, 'あｆｄｓ', '../images/145791972167623a631d82a8.26835951.jpg', '2024-12-18 02:58:43', 1, ''),
+(30, 4, 'あｆｄｓ', '../images/41308960067623a8615e9e3.92909094.jpg', '2024-12-18 02:59:18', 1, ''),
+(31, 3, 'あｆｄｓ', '../images/15210009967623aabae5fe6.16223308.jpg', '2024-12-18 02:59:56', 2, ''),
+(32, 3, 'あｆｄｓ', '../images/115578725767623af6859a40.17519938.jpg', '2024-12-18 03:01:11', 2, ''),
+(33, 4, 'あｆｄさｓふぁｄｆ', '', '2024-12-18 03:01:27', 1, ''),
+(34, 4, 'ｄｆｓだｓｆｄｆｓｄｆ', '', '2024-12-18 03:03:13', 3, '');
 
 -- --------------------------------------------------------
 
@@ -27734,6 +27799,12 @@ ALTER TABLE `cleaning_records`
   ADD PRIMARY KEY (`id`);
 
 --
+-- テーブルのインデックス `map`
+--
+ALTER TABLE `map`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- テーブルのインデックス `preset`
 --
 ALTER TABLE `preset`
@@ -27812,6 +27883,12 @@ ALTER TABLE `cleaning_records`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- テーブルの AUTO_INCREMENT `map`
+--
+ALTER TABLE `map`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
 -- テーブルの AUTO_INCREMENT `preset`
 --
 ALTER TABLE `preset`
@@ -27827,7 +27904,7 @@ ALTER TABLE `product`
 -- テーブルの AUTO_INCREMENT `survey_responses`
 --
 ALTER TABLE `survey_responses`
-  MODIFY `survey_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `survey_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- テーブルの AUTO_INCREMENT `test_time_change`
