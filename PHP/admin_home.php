ce28f600f55197517331dba1c02712a8d1338266
revers <emall@email.com>
2024-12-23 12:10:39 +0900

    あ

--- PHP/admin_home.php
+++ PHP/admin_home.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>管理者向け総合ページ</title>
+    <title>
+        <?php if(isset($title)) {
+                echo $title;
+                } else {
+                    echo "EcoPerks";
+                }
+         ?>
+    </title>
     <link rel="stylesheet" href="../CSS/hannbaka.css">
-
+    <link rel="stylesheet" href="../CSS/sidebar.css">
 </head>
 <body>
     <div class="left-menu">
@@ -28,6 +35,3 @@
         </div>
     </div>
 <div class="right-content">
-</ul>
-</body>
-</html>
