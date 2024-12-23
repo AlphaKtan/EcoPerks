f3f8790f491df64c9fb1aa7b6519fc47736813d7
revers <emall@email.com>
2024-12-23 12:25:24 +0900

    追加

--- PHP/admin_home.php
+++ PHP/admin_home.php
@@ -21,10 +21,10 @@
                 <p class="text-box">アカウント情報</p>
                 <li class="menu-item"><a href="admin.php" class="a_link"><img src="../img/hito.png" class="logo"><span class="menu-item-text">ログイン</span></a></li>
                 <p class="text-box">QRコード</p>
-                <li class="menu-item"><a href="adminqr.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">QRコード生成ページ</span></a></li>
-                <li class="menu-item"><a href="adminqr_end.php" class="a_link"><img src="../img/qr.png" class="logo"><span class="menu-item-text">QRコード終了管理</span></a></li>
+                <li class="menu-item"><a href="adminqr.php" class="a_link"><img src="../img/QR.svg" class="logo"><span class="menu-item-text">QRコード生成ページ</span></a></li>
+                <li class="menu-item"><a href="adminqr_end.php" class="a_link"><img src="../img/QR.svg" class="logo"><span class="menu-item-text">QRコード終了管理</span></a></li>
                 <p class="text-box">その他/管理・表示</p>
-                <li class="menu-item"><a href="data.php" class="a_link"><img src="../img/DB.png" class="logo"></span><span class="menu-item-text-chat">データ管理ページ</span></a></li>
+                <li class="menu-item"><a href="data.php" class="a_link"><img src="../img/dataDB.svg" class="logo"></span><span class="menu-item-text-chat">データ管理ページ</span></a></li>
                 <li class="menu-item"><a href="sankakanri.php" class="a_link"><img src="../img/tuika.png" class="logo"><span class="menu-item-text">参加者管理ページ</span></a></li>
                 <li class="menu-item"><a href="shift.php" class="a_link"><img src="../img/shift.png" class="logo"><span class="menu-item-text">シフトページ</span></a></li>
                 <li class="menu-item"><a href="time_change.php" class="a_link"><img src="../img/shiftsakujo.png" class="logo"><span class="menu-item-text">シフト削除</span></a></li>
--- PHP/create_qr.php
+++ PHP/create_qr.php
@@ -2,19 +2,29 @@
     // 仮の値を代入
     $area_id = 1;
     $location = 1;
+    // このページのタイトルを入力
+    $title = "QRコード生成";
+    include "admin_home.php";
 ?>
 <script src="../JS/qrcode.min.js"></script>
+<style>
+    div#qrcode {
+    width: fit-content;
+    padding: 25px;
+    background: white;
+    border-radius: 5%;
+}
+</style>
 <div id="qrcode"></div>
-<!-- <input type="text" id="text" placeholder="QRコードに変換するテキスト"> -->
-<button id="generate">QRコード生成</button>
 
 <script type="text/javascript">
     let area = "<?=$area_id; ?>";  // PHPの変数をjsの変数に代入
     let location_id = "<?=$location; ?>";
 </script>
 <script>
-    document.getElementById('generate').addEventListener('click', function() {
+    function createQR() {
         let fetchQR = document.getElementById('qrcode');
+        
         // QRコードがすでに生成されている場合は、削除
         fetchQR.innerHTML = "";
 
@@ -36,5 +46,14 @@
             colorLight: '#ffffff',
             correctLevel: QRCode.CorrectLevel.H
         });
-    });
+
+        fetchQR.removeAttribute("title");
+    }
+
+    createQR();
+
+    const interval = setInterval(() => {
+            createQR();
+                console.log("更新");
+            }, 3 * 60 * 1000);
 </script>
